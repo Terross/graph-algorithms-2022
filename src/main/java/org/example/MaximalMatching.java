86589c13dd64ec4c523c9b748562207a381f1593
Nata <96002623+Natanus2011@users.noreply.github.com>
2022-12-08 13:58:26 +0300

    Update and rename MyTask.java to MaximalMatching.java

--- src/main/java/org/example/MaximalMatching.java
+++ src/main/java/org/example/MaximalMatching.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
-public class MyTask implements GraphCharacteristic {
+public class MaximalMatching implements GraphCharacteristic {
     @Override
     public Integer execute(Graph graph) {
 
