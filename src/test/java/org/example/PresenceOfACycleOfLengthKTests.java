7c8c912fb627e616e25ed272ebad3bcd11091849
Gleb Pimenov <gleb.pimenov7@gmail.com>
2022-12-12 19:29:21 +0300

    Поправил названия тестов

--- src/test/java/org/example/PresenceOfACycleOfLengthKTests.java
+++ src/test/java/org/example/PresenceOfACycleOfLengthKTests.java
@@ -14,7 +14,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class PresenceOfACycleOfLengthKTests {
     @Test
     void TestOne() throws FileNotFoundException {
-        Graph testGraph = GraphFactory.loadGraphFromFile(new File("src/test/resources/graph1.txt"));
+        Graph testGraph = GraphFactory.loadGraphFromFile(new File("src/test/resources/PresenceOfACycleOfLengthKTest1.txt"));
         DirectedGraph G = new DirectedGraph(testGraph);
 
         assertThat(PresenceOfACycleOfLengthK.execute(G, 5)).isEqualTo(true);
@@ -22,70 +22,70 @@ public class PresenceOfACycleOfLengthKTests {
 
     @Test
     void TestTwo() throws FileNotFoundException {
-        Graph testGraph = GraphFactory.loadGraphFromFile(new File("src/test/resources/graph2.txt"));
+        Graph testGraph = GraphFactory.loadGraphFromFile(new File("src/test/resources/PresenceOfACycleOfLengthKTest2.txt"));
         DirectedGraph G = new DirectedGraph(testGraph);
 
         assertThat(PresenceOfACycleOfLengthK.execute(G, 13)).isEqualTo(false); //6
     }
     @Test
     void TestThree() throws FileNotFoundException {
-        Graph testGraph = GraphFactory.loadGraphFromFile(new File("src/test/resources/graph3.txt"));
+        Graph testGraph = GraphFactory.loadGraphFromFile(new File("src/test/resources/PresenceOfACycleOfLengthKTest3.txt"));
         DirectedGraph G = new DirectedGraph(testGraph);
 
         assertThat(PresenceOfACycleOfLengthK.execute(G, 4)).isEqualTo(false);
     }
     @Test
     void TestFour() throws FileNotFoundException {
-        Graph testGraph = GraphFactory.loadGraphFromFile(new File("src/test/resources/graph4.txt"));
+        Graph testGraph = GraphFactory.loadGraphFromFile(new File("src/test/resources/PresenceOfACycleOfLengthKTest4.txt"));
         DirectedGraph G = new DirectedGraph(testGraph);
 
         assertThat(PresenceOfACycleOfLengthK.execute(G, 12)).isEqualTo(true);
     }
     @Test
     void TestFive() throws FileNotFoundException {
-        Graph testGraph = GraphFactory.loadGraphFromFile(new File("src/test/resources/graph5.txt"));
+        Graph testGraph = GraphFactory.loadGraphFromFile(new File("src/test/resources/PresenceOfACycleOfLengthKTest5.txt"));
         DirectedGraph G = new DirectedGraph(testGraph);
 
         assertThat(PresenceOfACycleOfLengthK.execute(G, 7)).isEqualTo(false);
     }
     @Test
     void TestSix() throws FileNotFoundException {
-        Graph testGraph = GraphFactory.loadGraphFromFile(new File("src/test/resources/graph6.txt"));
+        Graph testGraph = GraphFactory.loadGraphFromFile(new File("src/test/resources/PresenceOfACycleOfLengthKTest6.txt"));
         DirectedGraph G = new DirectedGraph(testGraph);
 
         assertThat(PresenceOfACycleOfLengthK.execute(G, 3)).isEqualTo(false);
     }
     @Test
     void TestSeven() throws FileNotFoundException {
-        Graph testGraph = GraphFactory.loadGraphFromFile(new File("src/test/resources/graph7.txt"));
+        Graph testGraph = GraphFactory.loadGraphFromFile(new File("src/test/resources/PresenceOfACycleOfLengthKTest7.txt"));
         DirectedGraph G = new DirectedGraph(testGraph);
 
         assertThat(PresenceOfACycleOfLengthK.execute(G, 3)).isEqualTo(true);
     }
     @Test
     void TestEight() throws FileNotFoundException {
-        Graph testGraph = GraphFactory.loadGraphFromFile(new File("src/test/resources/graph8.txt"));
+        Graph testGraph = GraphFactory.loadGraphFromFile(new File("src/test/resources/PresenceOfACycleOfLengthKTest8.txt"));
         DirectedGraph G = new DirectedGraph(testGraph);
 
         assertThat(PresenceOfACycleOfLengthK.execute(G, 5)).isEqualTo(true);
     }
     @Test
     void TestNine() throws FileNotFoundException {
-        Graph testGraph = GraphFactory.loadGraphFromFile(new File("src/test/resources/graph9.txt"));
+        Graph testGraph = GraphFactory.loadGraphFromFile(new File("src/test/resources/PresenceOfACycleOfLengthKTest9.txt"));
         DirectedGraph G = new DirectedGraph(testGraph);
 
         assertThat(PresenceOfACycleOfLengthK.execute(G, 4)).isEqualTo(true);
     }
     @Test
     void TestTen() throws FileNotFoundException {
-        Graph testGraph = GraphFactory.loadGraphFromFile(new File("src/test/resources/graph10.txt"));
+        Graph testGraph = GraphFactory.loadGraphFromFile(new File("src/test/resources/PresenceOfACycleOfLengthKTest10.txt"));
         DirectedGraph G = new DirectedGraph(testGraph);
 
         assertThat(PresenceOfACycleOfLengthK.execute(G, 7)).isEqualTo(true);
     }
     @Test
     void TestEleven() throws FileNotFoundException {
-        Graph testGraph = GraphFactory.loadGraphFromFile(new File("src/test/resources/graph11.txt"));
+        Graph testGraph = GraphFactory.loadGraphFromFile(new File("src/test/resources/PresenceOfACycleOfLengthKTest11.txt"));
         DirectedGraph G = new DirectedGraph(testGraph);
 
         assertThat(PresenceOfACycleOfLengthK.execute(G, 9)).isEqualTo(true);
