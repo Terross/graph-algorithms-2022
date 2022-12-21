3557a675be25ac983a48fc1872fba455523b9815
golub <golubev1400@mail.ru>
2022-12-21 16:19:28 +0300

    дефицит

---
+++ src/main/java/BergeDeficite.java
@@ -0,0 +1,10 @@
+import com.mathsystem.api.graph.model.Graph;
+import com.mathsystem.domain.plugin.plugintype.GraphCharacteristic;
+
+public class BergeDeficite implements GraphCharacteristic {
+    @Override
+    public Integer execute(Graph graph) {
+        int vertCount = graph.getVertexCount();
+        return
+    }
+}
