e21a3780a2c5b1601721d5aa60d43fcc4d42adec
korotkovandrew <andrej_korotkov@mail.ru>
2022-12-24 19:51:15 +0300

    Example test added

---
+++ src/main/java/SomeModule.java
@@ -0,0 +1,9 @@
+import com.mathsystem.api.graph.model.Graph;
+import com.mathsystem.domain.plugin.plugintype.GraphProperty;
+
+public class SomeModule implements GraphProperty {
+    @Override
+    public boolean execute(Graph graph) {
+        return true;
+    }
+}
---
+++ src/test/java/SomeModuleTest.java
@@ -0,0 +1,25 @@
+
+import com.mathsystem.api.graph.GraphFactory;
+import com.mathsystem.api.graph.model.Graph;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.FileNotFoundException;
+import java.io.File;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+
+public class SomeModuleTest {
+    @Test
+    public void testSomeModule() throws FileNotFoundException {
+        Graph testGraph_1 = GraphFactory.loadGraphFromFile(new File("src/test/resources/graphs/someModule/testGraph_1.txt"));
+
+        SomeModule someModule = new SomeModule();
+        // assertThat(*результат вашего модуля над тестовыми графами*).isEqualTo(*ожидаемый результат*);
+        assertThat(someModule.execute(testGraph_1)).isTrue(); // пример
+        // assertThat(someModule.execute(testGraph_2)).isFalse();
+        // assertThat(3).isEqualTo(3);
+        // assertThat(3).isEqualTo(4); <- тест упадет
+    }
+}
