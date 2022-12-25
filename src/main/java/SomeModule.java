62563b76ec51857355d0808b7ae570c52f987c42
korotkovandrew <andrej_korotkov@mail.ru>
2022-12-25 17:36:26 +0300

    Example module deleted

--- src/main/java/SomeModule.java
+++
@@ -1,9 +0,0 @@
-import com.mathsystem.api.graph.model.Graph;
-import com.mathsystem.domain.plugin.plugintype.GraphProperty;
-
-public class SomeModule implements GraphProperty {
-    @Override
-    public boolean execute(Graph graph) {
-        return false;
-    }
-}
